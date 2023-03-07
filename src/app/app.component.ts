0f7e9128928e2992307473fba3b505edf8aca421
Semih Utku ÖZGAN <66949465+utk0z@users.noreply.github.com>
2023-03-07 11:23:26 +0300

    Learn Pipes on AngularJS

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  name:string='';
+  date:string='';
+  money:number=0;
+
+
+  onNameChange(value:string){
+    this.name = value;
+  }
+
+  onDateChange(value:string){
+    this.date = value;
+  }
+
+  onMoneyChange(value:string){
+    this.money = parseFloat(value);
+  }
+
+}
